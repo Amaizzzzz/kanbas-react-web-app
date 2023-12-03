99c64528e9ef177f3ff30d9f187a84ca39df9d8b
Amaizzzzz <112814055+Amaizzzzz@users.noreply.github.com>
2023-12-02 17:28:30 -0800

    update url

--- src/Labs/a5/AssignmentUpdater.js
+++ src/Labs/a5/AssignmentUpdater.js
@@ -4,7 +4,7 @@ function AssignmentUpdater() {
     const [score, setScore] = useState(0); // 0 is the initial value
     const [completed, setCompleted] = useState(false);
 
-    const baseUrl = "http://kanbas-node-server-app-gucen.onrender.com";
+    const baseUrl = "https://kanbas-node-server-app-gucen.onrender.com/";
 
     const handleScoreChange = (e) => {
         setScore(e.target.value);
